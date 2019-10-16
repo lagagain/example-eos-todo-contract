ea7cada2e5c338fb747037a91e52e0e98e1a7900
lagagain <lagagain@email.com>
2019-10-16 23:10:12 +0800

    Link to EOS Blockchain Network, but issue CORS

--- app/todopage/src/js/app.js
+++ app/todopage/src/js/app.js
@@ -2,6 +2,8 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import $ from "jquery";
 import 'bootstrap';
 //import Vue from 'vue';
+// import ScatterJS from '@scatterjs/core';
+// import ScatterEOS from '@scatterjs/eosjs';
 
 import config from './config.js';
 //let config = require("./config.js")
@@ -16,6 +18,33 @@ let app = new Vue({
     ]
   }
 });
+
+ScatterJS.plugins( new ScatterEOS() );
+const network = ScatterJS.Network.fromJson(config.config.network);
+
+
+function connect(){
+  ScatterJS.connect('todoapp', {network}).then(connected => {
+    if(!connected)
+      return false;
+  });
+}
+
+
+function login(){
+  if(! ScatterJS.login){
+      ScatterJS.login({accounts:[network]}).then(()=>{console.log("Login");});
+  }
+}
+
+$('#login').click((e)=>{
+  login();
+});
+
+$('#connect').click((e)=>{
+  connect();
+});
+
 
 
 //console.debug(config);
--- app/todopage/src/js/config.js
+++ app/todopage/src/js/config.js
@@ -1,6 +1,13 @@
 let config = {
-  network:"",
+  network:{
+    blockchain:'eos',
+    chainId:'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
+    host:'localhost',
+    port:8888,
+    protocol:'http'
+  },
 };
+
 
 module.exports = {
   config:config,
