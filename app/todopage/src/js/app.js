9b3651bc374646e3aee4f657258454b77b82e6cf
lagagain <lagagain@email.com>
2019-10-17 10:11:51 +0800

    Link to LocalNet

--- app/todopage/src/js/app.js
+++ app/todopage/src/js/app.js
@@ -2,8 +2,9 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import $ from "jquery";
 import 'bootstrap';
 //import Vue from 'vue';
-// import ScatterJS from '@scatterjs/core';
-// import ScatterEOS from '@scatterjs/eosjs';
+import ScatterJS from '@scatterjs/core';
+import ScatterEOS from '@scatterjs/eosjs';
+import {JsonRpc, Api} from 'eosjs';
 
 import config from './config.js';
 //let config = require("./config.js")
@@ -15,13 +16,14 @@ let app = new Vue({
       {id:1, task:"Example 1", status:"DONE"},
       {id:2, task:"Example 2", status:"DONE"},
       {id:3, task:"Example 3", status:"DONE"},
-    ]
+    ],
+    account:"",
   }
 });
-
 ScatterJS.plugins( new ScatterEOS() );
 const network = ScatterJS.Network.fromJson(config.config.network);
-
+const rpc = new JsonRpc(network.fullhost());
+const api = new Api({ rpc, signatureProvider:ScatterJS.scatter.eos});
 
 function connect(){
   ScatterJS.connect('todoapp', {network}).then(connected => {
@@ -30,21 +32,28 @@ function connect(){
   });
 }
 
-
 function login(){
-  if(! ScatterJS.login){
-      ScatterJS.login({accounts:[network]}).then(()=>{console.log("Login");});
-  }
+  let login = ScatterJS.login();
+  login.then((success)=>{
+    console.log("Login Success");
+    app.account = success.accounts[0];
+  },(fail)=>{
+    console.log("Login Fail");
+  });
 }
 
-$('#login').click((e)=>{
-  login();
-});
+function logout(){
+  ScatterJS.logout().then((e)=>{
+    console.log("Logout");
+    app.account="";
+  });
+}
 
-$('#connect').click((e)=>{
-  connect();
-});
+$('#login').click(login);
 
+$('#connect').click(connect);
+
+$('#logout').click(logout);
 
 
 //console.debug(config);
